4a64972f79549d09e250a392bb93a7ebca1a32fd
peabnuts123 <peabnuts123@gmail.com>
2024-04-11 09:56:20 +1200

    Increase v8 stack trace limit to better see Rust panics

--- src-web/src/main.ts
+++ src-web/src/main.ts
@@ -1,5 +1,9 @@
 import * as FantasyConsole from '@engine/fantasy_console';
 
+// @NOTE v8 custom property
+// Increase stack trace size for better view of Rust panics
+(Error as any).stackTraceLimit = 50;
+
 const somethingButton = document.querySelector("#something-button") as HTMLButtonElement;
 somethingButton?.addEventListener('click', () => {
   FantasyConsole.greet();
