d5b2ec843af9bd0b63c4fe96c10595126a723c54
peabnuts123 <peabnuts123@gmail.com>
2025-01-03 12:39:04 +1300

    Update PolyZone mock to implement latest commands

--- src/editor/src/web/lib/tauri/mock/modules/PolyZoneMockModule.ts
+++ src/editor/src/web/lib/tauri/mock/modules/PolyZoneMockModule.ts
@@ -1,6 +1,6 @@
 import { unzip, zip } from 'fflate';
 
-import { TauriCommandArgs } from '@lib/util/TauriCommands';
+import { TauriCommandArgs, TauriCommandReturnType } from '@lib/util/TauriCommands';
 
 import { Paths } from "../config";
 import { promisify, throwUnhandled } from '../util';
@@ -16,11 +16,15 @@ export class PolyZoneMockModule {
       case 'start_watching_project_files':
         return this.mockStartWatchingProjectFiles(args);
       case 'stop_watching_project_assets':
-        return this.mockStopWatchingProjectAssets(args);
+        return this.mockStopWatchingProjectAssets();
       case 'load_project':
         return this.mockLoadProject(args);
       case 'unload_project':
-        return this.mockUnloadProject(args);
+        return this.mockUnloadProject();
+      case 'hash_data':
+        throw this.mockHashData(args);
+      case 'set_path_is_busy':
+        return this.mockSetPathIsBusy(args);
       default:
         throw throwUnhandled(`[PolyZoneMockModule] (handle) Unimplemented action. (action='${action}') args: `, args);
     }
@@ -36,7 +40,7 @@ export class PolyZoneMockModule {
    * If you add or remove an asset the game likely won't even run. You will have to rebuild
    * the mock cartridge using the full app first.
    */
-  public static async mockCreateCartridge(args: TauriCommandArgs<'create_cartridge'>): Promise<Uint8Array> {
+  public static async mockCreateCartridge(...args: TauriCommandArgs<'create_cartridge'>): Promise<TauriCommandReturnType<'create_cartridge'>> {
     const { manifestFileBytes } = args[0];
     const result = await fetch(Paths.MockCartridgeFile);
     if (result.ok) {
@@ -44,25 +48,34 @@ export class PolyZoneMockModule {
       const cartridgeData = await unzipAsync(new Uint8Array(cartridgeBytes));
       cartridgeData['manifest.json'] = new TextEncoder().encode(manifestFileBytes);
       const resultBytes = await zipAsync(cartridgeData);
-      return resultBytes;
+      return Array.from(resultBytes);
     } else {
       throw throwUnhandled(`[PolyZoneMockModule] (create_cartridge) Failed fetching mock cartridge: `, result);
     }
   }
 
-  public static async mockLoadProject(_args: any) {
+  public static async mockLoadProject(..._args: TauriCommandArgs<'load_project'>): Promise<TauriCommandReturnType<'load_project'>> {
     // @NOTE No-op.
   }
 
-  public static async mockUnloadProject(_args: any) {
+  public static async mockUnloadProject(..._args: TauriCommandArgs<'unload_project'>): Promise<TauriCommandReturnType<'unload_project'>> {
     // @NOTE No-op.
   }
 
-  public static async mockStartWatchingProjectFiles({ }: TauriCommandArgs<'start_watching_project_files'>): Promise<void> {
+  public static async mockStartWatchingProjectFiles(...{ }: TauriCommandArgs<'start_watching_project_files'>): Promise<TauriCommandReturnType<'start_watching_project_files'>> {
     console.warn(`[PolyZoneMockModule] (start_watching_project_files) Tauri is mocked - project files will not be watched`);
+    return "This command is mocked";
   }
 
-  public static async mockStopWatchingProjectAssets({ }: TauriCommandArgs<'stop_watching_project_assets'>): Promise<void> {
+  public static async mockStopWatchingProjectAssets(...{ }: TauriCommandArgs<'stop_watching_project_assets'>): Promise<TauriCommandReturnType<'stop_watching_project_assets'>> {
+    // @NOTE No-op.
+  }
+
+  public static async mockHashData(..._args: TauriCommandArgs<'hash_data'>): Promise<TauriCommandReturnType<'hash_data'>> {
+    return '6f86d7a9413a517d';
+  }
+
+  public static async mockSetPathIsBusy(...args: TauriCommandArgs<'set_path_is_busy'>): Promise<TauriCommandReturnType<'set_path_is_busy'>> {
     // @NOTE No-op.
   }
 }
