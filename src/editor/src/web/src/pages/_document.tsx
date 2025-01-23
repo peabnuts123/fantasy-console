fc1aa14a8688f31b8856c0c11ab81edf34cb0651
peabnuts123 <peabnuts123@gmail.com>
2025-01-23 20:20:05 +1300

    Prevent overscroll in the app to make it seem more "desktop-y"

--- src/editor/src/web/src/pages/_document.tsx
+++ src/editor/src/web/src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="h-full">
+    <Html lang="en" className="h-full overscroll-none">
       <Head />
-      <body className="h-full">
+      <body className="h-full overscroll-none">
         <Main />
         <NextScript />
       </body>
